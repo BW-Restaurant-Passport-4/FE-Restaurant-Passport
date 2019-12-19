6f9a82b8e1a40735abe6687c1851c45b624db9a2
patrick-replogle <patrickr1138@gmail.com>
2019-12-18 20:14:59 -0800

    added local hook, context state management, axiosWithAuth and and private route.

--- src/App.js
+++ src/App.js
@@ -6,16 +6,22 @@ import Register from "./components/Register";
 import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
 import AddForm from "./components/AddForm";
+import UserProvider from "./providers/UserProvider";
+import PassportProvider from "./providers/PassportProvider";
 
 function App() {
   return (
-    <div className="App">
-      <Route exact path="/" component={WelcomePage} />
-      <Route path="/register" component={Register} />
-      <Route path="/login" component={Login} />
-      <Route path="/dashboard" component={Dashboard} />
-      <Route path="/add_form" component={AddForm} />
-    </div>
+    <UserProvider>
+      <PassportProvider>
+        <div className="App">
+          <Route exact path="/" component={WelcomePage} />
+          <Route path="/register" component={Register} />
+          <Route path="/login" component={Login} />
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/add_form" component={AddForm} />
+        </div>
+      </PassportProvider>
+    </UserProvider>
   );
 }
 
