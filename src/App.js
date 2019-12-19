526b7108fd8fcc4d28e220db5d65260919ff3c21
Sean Birmingham <seannbirmingham98@gmail.com>
2019-12-18 22:51:40 -0500

    added routes, added login/register buttons to welcome page
    Co-authored-by: Antoinette Jones <aajones71@gmail.com>
    Co-authored-by: Sean Birmingham <39016563+sean-birmingham@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
 import "./App.css";
+import { Route } from "react-router-dom";
+import WelcomePage from "./components/WelcomePage";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import Dashboard from "./components/Dashboard";
+import AddForm from "./components/AddForm";
 
 function App() {
   return (
     <div className="App">
-      <h1>App</h1>
+      <Route exact path="/" component={WelcomePage} />
+      <Route path="/register" component={Register} />
+      <Route path="/login" component={Login} />
+      <Route path="/dashboard" component={Dashboard} />
+      <Route path="/add_form" component={AddForm} />
     </div>
   );
 }
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,7 +3,7 @@ import React from "react";
 const Dashboard = props => {
   return (
     <div>
-      <h2>Add Form</h2>
+      <h2>Dashboard</h2>
     </div>
   );
 };
