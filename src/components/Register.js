84c5fc2ed82ef66b2820720adaa8a5e9228a8e8b
Sean Birmingham <seannbirmingham98@gmail.com>
2019-12-19 21:45:25 -0500

    changed title, set up form in register.js

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,11 +1,50 @@
 import React from "react";
+import { withFormik, Form, Field } from "formik"
+import { Link } from "react-router-dom";
 
 const Register = props => {
   return (
     <div>
       <h2>Register</h2>
+      <Form>
+        <label>
+          Name
+          <Field type="text" name="name" placeholder="Enter Name" />
+        </label>
+        <label>
+          Username
+          <Field type="text" name="username" placeholder="Enter Username" />
+        </label>
+        <label>
+          Email
+          <Field type="text" name="email" placeholder="Enter Email" />
+        </label>
+        <label>
+          Password
+          <Field type="text" name="password" placeholder="Enter Password" />
+        </label>
+        <label>
+          City
+          <Field type="text" name="city" placeholder="Enter Your City" />
+        </label>
+        <button type="submit">Submit</button>
+        <p>Already have an account? <Link to="/login">Login</Link></p>
+      </Form>
     </div>
   );
 };
 
-export default Register;
+const RegisterForm = withFormik({
+  mapPropsToValues(props) {
+    return {
+      name: props.name || "",
+      username: props.username || "",
+      email: props.email || "",
+      password: props.password || "",
+      city: props.city || ""
+    };
+
+  }
+})(Register)
+
+export default RegisterForm;
